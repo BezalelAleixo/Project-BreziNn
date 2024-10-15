19b5f868ca347471a8883b11cbc2f6ced5e41511
bezalel <bezalelaleixo@gmail.com>
2024-10-14 21:51:48 -0400

    Manos, eu fiz algumas alterações nos nomes dos códigos html, elas inicia com "cod", bom vou aproveitar o momento pra levantar algumas considerações, quando forem criar um novo código acrescentem no inicio o "cod" para deixar organizado, eu separei em pastas também o códigos, html, css, javascript e imagens. Por enquanto só tenho isso a dizer.

---
+++ Cod_BreziNn_JavaScript/Funçoes.js
@@ -0,0 +1,6 @@
+function criandoelemtos(){
+    for(i = 0 ; i<=10; i++){
+        const newelement  = "";
+        
+    }
+}
--- Funçoes.js
+++
@@ -1,6 +0,0 @@
-function criandoelemtos(){
-    for(i = 0 ; i<=10; i++){
-        const newelement  = "";
-        
-    }
-}
